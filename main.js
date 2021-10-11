394233cc590e5c9a2d7484c09993ff3ca0c29c33
riittagerhards <rgerhards@outlook.com>
2021-10-11 17:20:33 +0200

    Add conditional for buttons

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+const correctAnswer = true;
+
 const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Did it work?";
+
+const alertYes = document.querySelector(".yes");
+alertYes.onclick = function () {
+  if (correctAnswer === true) {
+    alert("Oh yes!");
+  } else {
+    alert("This is incorrect!!!");
+  }
+};
+
+const alertNo = document.querySelector(".no");
+alertNo.onclick = function () {
+  if (correctAnswer === false) {
+    alert("Oh yes!");
+  } else {
+    alert("This is incorrect!!!");
+  }
+};
