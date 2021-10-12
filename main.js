ac57f714e2d74cc2a5ad11831984b3adda502bf6
riittagerhards <rgerhards@outlook.com>
2021-10-12 11:05:49 +0200

    add new paragraph in js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const correctAnswer = true;
 
 const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Did it work?";
-
+/*
 const alertYes = document.querySelector(".yes");
 alertYes.onclick = function () {
   if (correctAnswer === true) {
@@ -20,3 +20,12 @@ alertNo.onclick = function () {
     alert("This is incorrect!!!");
   }
 };
+*/
+document.body.onload = myAnswer;
+function myAnswer() {
+  const newP = document.createElement("p");
+  const newAnswer = document.createTextNode("Oh yes, this is correct!");
+  newP.appendChild(newAnswer);
+  const currentP = document.getElementById("emptyP");
+  document.body.insertBefore(newP, emptyP);
+}
